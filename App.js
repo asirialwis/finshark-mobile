21c3081ebee20244abb9e1c4246e6d01e1aa3498
asirialwis <asirichathurika900@gmail.com>
2024-08-15 18:59:28 +0530

    import required libs

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-
+import React, { useState, useEffect } from 'react';
+import { View, Text, TextInput, Button, StyleSheet, ScrollView } from 'react-native';
+import { initializeApp } from '@firebase/app';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
