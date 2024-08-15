8df3bac44779bbbcb266760969b9feb283d9eea6
asirialwis <asirichathurika900@gmail.com>
2024-08-15 18:50:17 +0530

    add firebase config

--- App.js
+++ App.js
@@ -1,6 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBtzKJSU7FgABAEl9AX-6ty-JbVzMlCGP4",
+  authDomain: "fir-auth-tutorial-23abd.firebaseapp.com",
+  projectId: "fir-auth-tutorial-23abd",
+  storageBucket: "fir-auth-tutorial-23abd.appspot.com",
+  messagingSenderId: "438234252998",
+  appId: "1:438234252998:web:c26df2722e0a709b3b39c7",
+  measurementId: "G-46MG7BQE7E"
+
+};
+
+
 export default function App() {
   return (
     <View style={styles.container}>
